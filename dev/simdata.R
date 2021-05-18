5370f46d26d8a82f661ce63e7e4431dcffdffc5a
Rafael Henkin <rafael.henkin@gmail.com>
2021-05-18 11:19:23 +0100

    Example data

--- dev/simdata.R
+++ dev/simdata.R
@@ -1,3 +1,23 @@
+#Simulate normal data
+normal_random <- lapply(1:10, function(x) {
+  rnorm(200, runif(1, -5, 5), runif(1, 0, 10))
+})
+normal_df <- data.frame(normal_random)
+colnames(normal_df) <- letters[1:10]
+write.csv(normal_df, "dev/sim_normal.csv", row.names = FALSE)
+file.copy("dev/sim_normal.csv", "inst/www/data/sim_normal.csv", overwrite = TRUE)
+save(normal_df, file = "data/sim_normal.rda")
+
+# Add annotation
+normal_annotated <- cbind(normal_df, "annot" = rep(c("A", "B"), each = 100))
+write.csv(normal_annotated, "dev/sim_normalannot.csv", row.names = FALSE)
+file.copy("dev/sim_normalannot.csv", "inst/www/data/sim_normalannot.csv", overwrite = TRUE)
+save(normal_annotated, file = "data/sim_normalannot.rda")
+normal_missing <- normal_df
+normal_missing[sample(200, 50), "j"] <- NA
+write.csv(normal_missing, "dev/sim_normal_missing.csv", row.names = FALSE)
+file.copy("dev/sim_normal_missing.csv", "inst/www/data/sim_normal_missing.csv", overwrite = TRUE)
+save(normal_missing, file = "data/sim_normal_missing.rda")
 # Simulate binary data
 bin_randoms <- lapply(1:10, function(x) {
   x <- qnorm(runif(1,min=pnorm(0),max=pnorm(1)))[1]
@@ -6,9 +26,13 @@ bin_randoms <- lapply(1:10, function(x) {
 bin_df <- data.frame(bin_randoms)
 colnames(bin_df) <- letters[1:10]
 write.csv(bin_df, "dev/sim_binary.csv", row.names = FALSE)
+file.copy("dev/sim_binary.csv", "inst/www/data/sim_binary.csv", overwrite = TRUE)
+save(bin_df, file = "data/sim_binary.rda")
 # Simulate log scaled data
 lambdas <- sample(1:10, 10)
 randoms <- lapply(lambdas, function(x) log1p(rpois(500, x)))
-df <- data.frame(randoms)
-colnames(df) <- letters[1:10]
-write.csv(df, "dev/sim_logscaled.csv", row.names = FALSE)
+logscaled_df <- data.frame(randoms)
+colnames(logscaled_df) <- letters[1:10]
+write.csv(logscaled_df, "dev/sim_logscaled.csv", row.names = FALSE)
+file.copy("dev/sim_logscaled.csv", "inst/www/data/sim_logscaled.csv", overwrite = TRUE)
+save(logscaled_df, file = "data/sim_logscaled.rda")
--- inst/helpfiles/sidebar_help.md
+++ inst/helpfiles/sidebar_help.md
@@ -7,8 +7,18 @@ This sidebar controls the main clustering workflow:
 1. Select the number of desired clusters
 1. Select the variables/features to use in clustering
 
-Most tabs will be updated with the results corresponding to the current settings. The sidebar contains two additional controls to help with the analysis:
+Most tabs will be updated without any additional inputs. The sidebar contains two other controls to help with the analysis:
 
 *Heatmap annotations:* any categorical variable loaded with the data will be available for selection, as well as the numeric variables that contain missing values.
 
 *Correlation-based feature selection:* a one-sided t-test based on the chosen threshold will be use to automatically remove highly correlated variables. You can inspect the relationship between the variables that were deselected in the *Data overview* tab, under *Highly correlated variables*.
+
+### Example data
+
+The following datasets can be downloaded, inspected and loaded into the tool to understand the required format:  
+
+1. [Normally-distributed data](data/sim_normal.csv)  
+1. [Normally-distributed data with missing values](data/sim_normal_missing.csv)  
+1. [Normally-distributed data with annotation](data/sim_normalannot.csv)  
+1. [Binary data](data/sim_binary.csv)  
+1. [Log-scaled data](data/logscaled.csv)  
